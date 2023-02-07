0f2e1262354904610b2ebc635ea0cc71bf1d260d
Satyam Pandey Vapi <113432841+pandey-satyam-0@users.noreply.github.com>
2023-02-07 12:14:57 +0530

    h-scroll with gsap & scrollTrigger

---
+++ script.js
@@ -0,0 +1,54 @@
+const horizontal = document.querySelector("#horizontal");
+const sections = gsap.utils.toArray("#horizontal section");
+const texts = gsap.utils.toArray(".anim");
+const mask = document.querySelector(".mask");
+
+let scrollTween = gsap.to(sections, {
+    xPercent: -100 * (sections.length - 1),
+    ease: "none",
+    scrollTrigger: {
+        trigger: "#horizontal",
+        pin: true,
+        scrub: 1,
+        end: "+=3000",
+        //snap: 1 / (sections.length - 1),
+        markers: false,
+    }
+});
+
+console.log(1 / (sections.length - 1))
+
+//Progress bar animation
+
+gsap.to(mask, {
+    width: "100%",
+    scrollTrigger: {
+        trigger: ".wrapper-scroll",
+        start: "top left",
+        scrub: 1
+    }
+});
+
+// whizz around the sections
+sections.forEach((section) => {
+    // grab the scoped text
+    let text = section.querySelectorAll(".anim");
+
+    // bump out if there's no items to animate
+    if (text.length === 0) return
+
+    // do a little stagger
+    gsap.from(text, {
+        y: -130,
+        opacity: 0,
+        duration: 2,
+        ease: "elastic",
+        stagger: 0.1,
+        scrollTrigger: {
+            trigger: section,
+            containerAnimation: scrollTween,
+            start: "left center",
+            markers: false
+        }
+    });
+});
